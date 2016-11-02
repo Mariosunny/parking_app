cf90a4e292e205886c1b65466fcdccae5e809817
gnosistommy <gnosistommy@yahoo.com>
2016-11-01 19:58:42 -0500

    11/1 second edit
    chopped up video, and altered recognition software to play all four
    videos.

--- server/project/app/main_app/vehicle_detection.py
+++ server/project/app/main_app/vehicle_detection.py
@@ -8,26 +8,33 @@ cv2.ocl.setUseOpenCL(False)
 
 
 fgbg = cv2.createBackgroundSubtractorMOG2()
-car_cascade = cv2.CascadeClassifier('carcascade4.xml')
-cap = cv2.VideoCapture("test.mp4")
-count=0
-while True:
-    ret, img = cap.read()
+car_cascade = cv2.CascadeClassifier('cascades/carcascade4.xml')
+i=1
+while i<5:
+    cap = cv2.VideoCapture("videos/test"+str(i)+".mp4")
+    count=0
+    county=0
+    while True:
+        ret, img = cap.read()
     
-    if (type(img) == type(None)):
-        break
-    #fgmask = fgbg.apply(img)
-    #gray = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(img,(5,5),0)
-    cars = car_cascade.detectMultiScale(blur,7.5,4)
+        if (type(img) == type(None)):
+            break
+        #fgmask = fgbg.apply(img)
+        #gray = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(img,(5,5),0)
+        cars = car_cascade.detectMultiScale(blur,7.5,4)
     
-    for (x,y,w,h) in cars[0:1]:
-        if(w>50):
-            cv2.rectangle(img,(x,y),(x+w,y+h),(0,250,0),2)      
+        for (x,y,w,h) in cars[0:1]:
+            if w>50 and (count%2==0) and county<5:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,250,0),2)
+                county += 1
     
-    cv2.imshow('video', img)
-    count += count + 1
-    if cv2.waitKey(33) == 27:
-        break
-
+        cv2.imshow('video', img)
+        count += 1
+                
+        if cv2.waitKey(33) == 27:
+            break
+    i += 1   
+    print(str(county))
+    
 cv2.destroyAllWindows()
