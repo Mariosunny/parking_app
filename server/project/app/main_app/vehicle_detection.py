1fb92e20913faa82eaa4c6b4abcdb804656c5286
gnosistommy <gnosistommy@yahoo.com>
2016-11-10 15:26:44 -0600

    11/10 edit
    brought resolution of videos down, still fine tuning cascades

--- server/project/app/main_app/vehicle_detection.py
+++ server/project/app/main_app/vehicle_detection.py
@@ -1,40 +1,52 @@
-# -*- coding: utf-8 -*-
 import numpy as np
 import cv2
 cv2.ocl.setUseOpenCL(False)
 
-#cascade_src = 'cars.xml'
-#video_src = 'dataset/test3.mp4'
 
-
-fgbg = cv2.createBackgroundSubtractorMOG2()
 car_cascade = cv2.CascadeClassifier('cascades/carcascade4.xml')
 i=1
-while i<5:
-    cap = cv2.VideoCapture("videos/test"+str(i)+".mp4")
+while i<6:
+    cap = cv2.VideoCapture("videos/low_res_tests"+str(i)+".mp4")
     count=0
     county=0
+    countys=0
+    carsin=0
+    carsout=0
+    prev_x=0
+    a=100
+    b=220
+    first=0
     while True:
         ret, img = cap.read()
     
         if (type(img) == type(None)):
             break
-        #fgmask = fgbg.apply(img)
-        #gray = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(img,(5,5),0)
-        cars = car_cascade.detectMultiScale(blur,7.5,4)
-    
-        for (x,y,w,h) in cars[0:1]:
-            if w>50 and (count%2==0) and county<5:
-                cv2.rectangle(img,(x,y),(x+w,y+h),(0,250,0),2)
-                county += 1
-    
+        
+        blur = cv2.GaussianBlur(img,(3,3),0)
+        cars = car_cascade.detectMultiScale(blur,1.1,5)
+        cv2.line(img, (a, 0),(a,400),  (250,0,0),2)
+        cv2.line(img, (b, 0),(b,400),  (250,0,0),2)
+        for (x,y,w,h) in cars[0:2]:
+            if x<a:
+                countys += 1
+                cv2.rectangle(img,(x,50),(x+100,120),(0,250,0),2)
+                carsin += 1
+                countys=0
+                first=0
+            elif x>b:
+                countys += 1
+                cv2.rectangle(img,(x,50),(x+100,120),(0,250,0),2)
+                carsout += 1
+            
+            print(str(prev_x) + " " + str(x))
+            
         cv2.imshow('video', img)
         count += 1
                 
         if cv2.waitKey(33) == 27:
             break
+    prev_x=0
     i += 1   
-    print(str(county))
+    print(str(count) + " frames, " + str(carsin) + " car(s) in" + str(carsout) + " car(s) out")
     
 cv2.destroyAllWindows()
